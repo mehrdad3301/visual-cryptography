53c1a7645246eafd1892d2449f06053b02c12df3
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-06 22:33:11 +0330

    FIX

--- src/main.go
+++ src/main.go
@@ -40,7 +40,7 @@ func setShare(transparent *image.Gray , share int , x , y , c int) {
 	for i := 0 ; i < c ; i++ { 
 		for j := 0 ; j < c ; j++ { 
 
-			bit := utils.getKthBit(share , i*c + j)	
+			bit := utils.GetKthBit(share , i*c + j)	
 			clr := color.White 
 			if bit == 1 { 
 				clr = color.Black
