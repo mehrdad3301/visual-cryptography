2e425f8abe6ac894fc070a17123c8d89129e1c4b
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-07 12:40:50 +0330

    REFACTOR: moved image handling implmentation to utils/images.go

--- src/merger.go
+++ src/merger.go
@@ -4,43 +4,10 @@ package main
 import ( 
 	"os"
 	"image"
-	"image/png"
 	"image/color"
-	"log"
+	"utils"
 ) 
 	
-
-
-func openImages(names []string) []image.Image { 
-
-	images := make([]image.Image , 0) 
-	for _ , name := range(names) { 
-
-		file , err := os.Open(name)
-		if err != nil { 
-			log.Fatal(err) 
-		}
-		
-		image , _  , err := image.Decode(file)
-		if err != nil { 
-			log.Fatal(err) 
-		}
-
-		images = append(images , image) 	
-		file.Close() 
-	}
-	return images 
-}
-
-func isBlack(c color.Color) bool { 
-	r , g , b, a := c.RGBA() 
-	if (r == 0) && (b == 0) &&
-	   (g == 0) && (a >> 8 == 255) { 
-		return true 
-	}
-	return false
-}
-
 func merge(images []image.Image) { 
 	
 	startPoint , endPoint := images[0].Bounds().Min , images[0].Bounds().Max
@@ -50,30 +17,19 @@ func merge(images []image.Image) {
 		for y := startPoint.Y ; y < endPoint.Y ; y++ { 
 			c := color.White 
 			for _ , image := range(images) { 
-				if isBlack(image.At(x , y)) { 
+				if utils.IsBlack(image.At(x , y)) { 
 					c = color.Black 
 				}
 			}
 			mergedImage.Set(x , y , c) 	
 		}
 	}
-	writeImage("merged.png" , mergedImage) 
-} 
-
-func writeImage(filename string , img *image.Gray) { 
-
-	file , err := os.Create(filename) 
-	if err != nil { 
-		log.Fatal(err) 
-	} 
-	defer file.Close()
-
-	png.Encode(file , img) 
+	utils.WriteImage("merged.png" , mergedImage) 
 } 
 
 func main() { 
 
 	imgNames := os.Args[1:] 
-	images := openImages(imgNames) 
+	images := utils.ReadImages(imgNames) 
 	merge(images) 
 } 
