e2bbf6adde6845cbc75fb798daa85966eb65d0ce
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-10-08 11:41:47 +0330

    UPDATE

--- src/viscrypt/main.go
+++ src/viscrypt/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"flag"
 	"os"
 	"viscrypt/single"
+	"viscrypt/multi"
 )
 
 func main() {
 
 	n := flag.Int("n" , 2 , "number of transparencies") 
-	multi := flag.Bool("multi-mode" , false , "wether to use multi mode") 
+	isMulti := flag.Bool("multi-mode" , false , "wether to use multi-mode") 
+	flag.Parse()
 	
-	if *multi == false {
+	if *isMulti == false {
 		single.Encrypt(os.Args[3] , *n)
+	} else { 
+		fmt.Println(os.Args[3:])
+		multi.Encrypt(os.Args[3:])
 	}
 }
